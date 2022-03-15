2e9c1e7c140d2819a038d6a4b61eb57ac39caf8c
Carla Alicia Morales Pacheco <83970319+carlamorales@users.noreply.github.com>
2022-03-14 23:03:53 -0300

    Update Contents.swift

--- ejercicio6.playground/Contents.swift
+++ ejercicio6.playground/Contents.swift
@@ -1,137 +1,172 @@
-// protocolos:
-protocol Encage { // pasar a estructura de animales voladores y terrestres
-    var estaDentro: Bool { get }
-    func enjaular() -> Bool
+enum AnimalType {
+    case air
+    case land
+    case sea
 }
-protocol Entanke { // pasar a estructura de animales marinos.
-    var estaDentro: Bool { get }
-    func toPutInTank() -> Bool
+
+protocol Animal {
+    var type: AnimalType { get }
 }
-protocol WalkOut { // pasar a la estructura de animales terrestres.
-    func takeForAWalk()
+protocol Cageable {
+    func getInCage()
+    func setFree()
 }
-protocol Feed { // pasar a todas las estructuras de animales.
-    var itsFeed: Bool { get }
-    func toFeed()
+protocol Tankeable {
+    func getInTanke()
+    func setFree()
 }
-protocol Clean { // pasar a todas las estructuras de jaulas.
-    var itsClean: Bool {get}
-    func toClean()
+protocol Feedable {
+    func feed()
 }
+protocol Walkable {
+    func takeOutForWalk()
+}
+protocol Cleanable {
+    func cleanThisPlace()
+}
+
+typealias CageableAndWalkableAnimal = Cageable & Walkable & Animal
+typealias CageableAnimal = Cageable & Animal
+typealias TankeableAnimal = Tankeable & Animal
 
 // estructuras animales:
-struct FlyingAnimal: Feed, Encage {
-    var estaDentro: Bool
-    var type: String
-    var itsFeed: Bool
-
-    func enjaular() -> Bool{
-        return estaDentro
-    }
-
-    func liberar() -> Bool {
-        return estaDentro
-    }
-
-    func toFeed() {
-        if itsFeed {
-            print("Ok. No need to feed.")
-        } else {
-            print("You need to feed it.")
-        }
-    }
-}
-
-struct LandAnimal: Feed, Encage {
-    var estaDentro: Bool
+struct LandAnimal: CageableAndWalkableAnimal, Feedable {
+    var type: AnimalType
+    var name: String
     
-    func enjaular() -> Bool {
-        print("enjaulado")
-        return true
+    func getInCage() {
+        print("The \(name) is in the cage.")
     }
     
-    var type: String
-    var itsFeed: Bool
-    func toFeed() {
-        if itsFeed {
-            print("Ok. No need to feed.")
-        } else {
-            print("You need to feed it.")
-        }
-    }
-}
-
-struct SeaAnimal: Feed, Entanke {
-    var type: String
-    var estaDentro: Bool
-    func toPutInTank() -> Bool {
-        print("en tanke")
-        return true
+    func setFree() {
+        print("The \(name) is out of the cage.")
     }
     
-    var itsFeed: Bool
-    func toFeed() {
-        if itsFeed {
-            print("Ok. No need to feed.")
-        } else {
-            print("You need to feed it.")
-        }
+    func takeOutForWalk() {
+        print("This \(name) is being taken out for a walk.")
     }
-}
-// instancias animales:
-var parrot1 = FlyingAnimal(estaDentro: true, type: "parrot", itsFeed: true)
-parrot1.toFeed()
-var cat1 = LandAnimal(estaDentro: true, type: "cat", itsFeed: false)
-var goldfish1 = SeaAnimal(type: "goldfish", estaDentro: true, itsFeed: false)
-var pezpayaso = SeaAnimal(type: "pesPayaso", estaDentro: true, itsFeed: false)
-var pulpo = SeaAnimal(type: "pulpo", estaDentro: true, itsFeed: false)
-
-// estructura jaula:
-struct Cage: Clean {
-    var occupant : [Encage] = []
-    var itsClean: Bool
-
-    func toClean() {
-        if itsClean {
-            print("no se necesita limpiar")
-        } else {
-            print("se necesita limpiar")
-        }
-    }
-}
-// estructura tanque de peces:
-struct TankSeaAnimals: Clean {
-    var occupant: [Entanke] = []
-
-    var itsClean : Bool
-    func toClean() {
-        if itsClean {
-            print("no se necesita limpiar")
-        } else {
-            print("se necesita limpiar")
-        }
+    
+    func feed() {
+        print("This \(name) is being fed.")
     }
 }
 
-// instancias de la jaula:
-var cage1 = Cage(itsClean: true)
-cage1.occupant.append(parrot1)
-cage1.occupant.append(cat1)
-print("Hay \(cage1.occupant.count) animales en esta jaula.")
-cage1.occupant.remove(at: 0)
-print("Hay \(cage1.occupant.count) animales en esta jaula.")
-
-// instancias del tanque:
-var tank1 = TankSeaAnimals(itsClean: false)
-tank1.occupant.append(goldfish1)
-tank1.occupant.append(pezpayaso)
-tank1.occupant.append(pulpo)
-print("Hay \(tank1.occupant.count) peces en este tanque.")
-tank1.occupant.remove(at: 2)
-print("Hay \(tank1.occupant.count) peces en este tanque.")
-
-//// estructura tienda:
-struct PetShop {
-    var accommodationCage: Cage
-    var accommodationTank: TankSeaAnimals
+struct AirAnimal: CageableAnimal, Feedable {
+    var type: AnimalType
+    var name: String
+    
+    func getInCage() {
+        print("The \(name) is in the cage.")
+    }
+    
+    func setFree() {
+        print("The \(name) is out of the cage.")
+    }
+    
+    func feed() {
+        print("This \(name) is being fed.")
+    }
 }
+
+struct SeaAnimal: TankeableAnimal, Feedable {
+    var type: AnimalType
+    var name: String
+    
+    func getInTanke() {
+        print("The \(name) is in the tank.")
+    }
+    
+    func setFree() {
+        print("The \(name) is out of the tank.")
+    }
+    
+    func feed() {
+        print("This \(name) is being fed.")
+    }
+}
+
+// instancias de las estructuras animales:
+var dog = LandAnimal(type: .land, name: "dog")
+dog.feed()
+var cat = LandAnimal(type: .land, name: "cat")
+cat.feed()
+
+var parrot = AirAnimal(type: .air, name: "parrot")
+parrot.feed()
+var duck = AirAnimal(type: .air, name: "duck")
+duck.feed()
+
+var goldfish = SeaAnimal(type: .sea, name: "goldfish")
+goldfish.feed()
+var clownfish = SeaAnimal(type: .sea, name: "clownfish")
+clownfish.feed()
+
+// estructuras jaulas:
+struct LandAnimalsCage: Cleanable {
+    var occupants: [CageableAndWalkableAnimal] = []
+    
+    mutating func cageLandAnimal(animalName: CageableAndWalkableAnimal) {
+        occupants.append(animalName)
+        animalName.getInCage()
+    }
+    
+    /*
+    func setLandAnimalFree()
+    */
+    
+    func cleanThisPlace() {
+        print("This place is being cleaned.")
+    }
+}
+
+struct AirAnimalsCage: Cleanable {
+    var occupants: [CageableAnimal] = []
+    
+    mutating func cageAirAnimal(animalName: CageableAnimal) {
+        occupants.append(animalName)
+        animalName.getInCage()
+    }
+    
+    /*
+    func setAirAnimalFree()
+    */
+    
+    func cleanThisPlace() {
+        print("This place is being cleaned.")
+    }
+}
+
+struct Tank: Cleanable {
+    var occupants: [TankeableAnimal] = []
+    
+    mutating func putSeaAnimalInTank(animalName: TankeableAnimal) {
+        occupants.append(animalName)
+        animalName.getInTanke()
+    }
+    
+    /*
+    func setSeaAnimalFree()
+    */
+    
+    func cleanThisPlace() {
+        print("This place is being cleaned.")
+    }
+}
+
+// instancias de las estructuras jaulas:
+var landAnimalCage = LandAnimalsCage()
+landAnimalCage.cleanThisPlace()
+var airAnimalCage = AirAnimalsCage()
+airAnimalCage.cleanThisPlace()
+var tank = Tank()
+tank.cleanThisPlace()
+
+// llamada de método meter a jaula/tanque:
+landAnimalCage.cageLandAnimal(animalName: dog)
+landAnimalCage.cageLandAnimal(animalName: cat)
+
+airAnimalCage.cageAirAnimal(animalName: parrot)
+airAnimalCage.cageAirAnimal(animalName: duck)
+
+tank.putSeaAnimalInTank(animalName: goldfish)
+tank.putSeaAnimalInTank(animalName: clownfish)
